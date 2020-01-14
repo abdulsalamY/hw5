b42b239b13dbd33a0a57a43be90b3fb08548bc12
abdulsalamY <59774246+abdulsalamY@users.noreply.github.com>
2020-01-14 04:33:39 +0200

    .gitignore added

---
+++ RegisterManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by abdul on 1/14/2020.
+//
+
+#include "RegisterManager.hpp"
---
+++ RegisterManager.hpp
@@ -0,0 +1,14 @@
+//
+// Created by abdul on 1/14/2020.
+//
+
+#ifndef HW5_REGISTERMANAGER_HPP
+#define HW5_REGISTERMANAGER_HPP
+
+
+class RegisterManager {
+
+};
+
+
+#endif //HW5_REGISTERMANAGER_HPP
